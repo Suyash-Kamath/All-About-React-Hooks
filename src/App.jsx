5e323bafe6dcaf804e084fda95d20224e254358c
Suyash Kamath <suyash@Mac.lan>
2024-10-10 11:36:29 +0530

    All about React Hooks

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import UseStateHooks from './hooks/UseStateHooks'
+import UseEffectHooks from './hooks/UseEffectHooks'
+import UseRefHooks from './hooks/UseRefHooks'
+import UseMemoHooks from './hooks/UseMemoHooks'
+import UseCallbackHooks from './hooks/UseCallbackHooks'
+import UseContextHooks from './hooks/UseContextHooks'
+import UseReducerHook from './hooks/UseReducerHook'
+import UseLayoutEffectHook from './hooks/UseLayoutHookEffect'
+import CustomHook from './hooks/CustomHook'
+function App() {
+
+   return (
+   //  <UseStateHooks/>
+   // <UseEffectHooks/>
+   // <UseMemoHooks/>
+   // <UseCallbackHooks/>
+   // <UseContextHooks/>
+   // <UseLayoutEffectHook/>
+   <CustomHook/>
+
+   )
+}
+
+export default App
